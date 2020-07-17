7a7eab51d8ad7291b78c892174a88228389dc544
Kishor Naik <kishor.naik011.net@outlook.com>
2020-07-17 16:24:26 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sol_Demo.Models;
+
+namespace Sol_Demo.Controllers
+{
+    public class UsersController : Controller
+    {
+        [BindProperty]
+        public UserModel Users { get; set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("onsubmit", Name = "onsubmit")]
+        public IActionResult Submit()
+        {
+            if (base.ModelState.IsValid)
+            {
+                return Content("Validation Pass");
+            }
+            else
+            {
+                return View("Index", Users);
+            }
+        }
+    }
+}
---
+++ Sol_Demo/Sol_Demo/Models/UserModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Sol_Demo.Models
+{
+    public class UserModel
+    {
+        // https://docs.microsoft.com/en-us/aspnet/core/mvc/models/validation?view=aspnetcore-3.1
+        // Pre Defined Data Annotations
+        [Required]
+        [MaxLength(50, ErrorMessage = "First Name should be less than 50 character")]
+        [DisplayName("First Name")]
+        public String FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "Last Name should be less than 50 character")]
+        [DisplayName("Last Name")]
+        public String LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [DisplayName("Email Id")]
+        public String EmailId { get; set; }
+
+        [Required]
+        [MaxLength(10, ErrorMessage = "10 digit no allowed only")]
+        [MobileNoValidation]
+        [DisplayName("Mobile No")]
+        public String MobileNo { get; set; }
+
+        private class MobileNoValidation : ValidationAttribute
+        {
+            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+            {
+                ValidationResult validateResult = null;
+
+                if (Convert.ToString(value).StartsWith("0"))
+                {
+                    validateResult = new ValidationResult("Mobile no does not start with 0");
+                }
+                else if (Convert.ToString(value).Length != 10)
+                {
+                    validateResult = new ValidationResult("10 digit no allowed only");
+                }
+                else
+                {
+                    validateResult = ValidationResult.Success;
+                }
+                //else if (Convert.ToString(value).Length != 10)
+                //{
+                //    validateResult = new ValidationResult("10 digit number allowed only");
+                //}
+
+                return validateResult;
+            }
+        }
+    }
+}
